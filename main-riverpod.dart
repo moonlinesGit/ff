0d19e58920dd82dc9bd8cbd3eb1a990553652b8d
moonlinesGit <145001548+moonlinesGit@users.noreply.github.com>
2025-08-11 12:59:34 +0300

    Update main-riverpod.dart

--- main-riverpod.dart
+++ main-riverpod.dart
@@ -2,97 +2,119 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('myBox');
-  runApp(const ProviderScope(child: MyApp()) );
+  await Hive.openBox('cartBox');
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Navigation Demo',
-      /* home: HomeScreen(),  */
-      home:const Main(), 
+      home: const Main(),
     );
   }
 }
-final listProvider= StateProvider<List<Map<String, String>>>((ref)=>[]);
 
+final cartProvider = StateNotifierProvider<CartNotifier, List<String>>((ref) {
+  return CartNotifier();
+});
 
-class Main extends ConsumerStatefulWidget{
+class CartNotifier extends StateNotifier<List<String>> {
+  CartNotifier() : super([]) {
+    loadFromHive();
+  }
+
+  void loadFromHive() {
+    final box = Hive.box('cartBox');
+    final savedItems = box.get('items', defaultValue: <String>[]);
+    state = List<String>.from(savedItems); // cast to List<String>
+  }
+
+  void addItem(String item) {
+    state = [...state, item]; // semicolon added
+    final box = Hive.box('cartBox');
+    box.put('items', state);
+  }
+
+  void deleteItem(String item) {
+    state = state.where((el) => el != item).toList();
+    final box = Hive.box('cartBox');
+    box.put('items', state);
+  }
+}
+
+class Main extends ConsumerStatefulWidget {
   const Main({super.key});
 
   @override
-  ConsumerState<Main> createState()=> _MainState();
+  ConsumerState<Main> createState() => _MainState();
 }
 
-class _MainState extends ConsumerState<Main>{
+class _MainState extends ConsumerState<Main> {
+  @override
+  Widget build(BuildContext context) {
+    final cartList = ref.watch(cartProvider);
+    return Scaffold(
+      body: Column(children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: cartList.length,
+              itemBuilder: (context, index) {
+                final item = cartList[index];
+                return ListTile(title: Text('$item'));
+              }),
+        ),
+        ElevatedButton(
+          child: const Text('Go to Modifier'),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ModifyCart(),
+                ));
+          },
+        )
+      ]),
+    );
+  }
+}
+
+class ModifyCart extends ConsumerStatefulWidget {
+  const ModifyCart({super.key});
 
   @override
-  Widget build(BuildContext context){
-    final list=ref.watch(listProvider);
-    final box = Hive.box('myBox');
-    final name=box.get('task');
+  ConsumerState<ModifyCart> createState() => _ModifyCartState();
+}
+
+class _ModifyCartState extends ConsumerState<ModifyCart> {
+  void addItem(String item) {
+    ref.read(cartProvider.notifier).addItem(item);
+  }
+
+  void deleteItem(String item) {
+    ref.read(cartProvider.notifier).deleteItem(item);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        children:[
-          Expanded(
-            child: ListView.builder(
-              itemCount:list.length,
-              itemBuilder: (context,index)
-                {
-                  final item = list[index];
-                  final key = item.keys.first;
-                  final value = item.values.first;
-                  return ListTile(title:Text('$key'),trailing:Text('$value'));
-                })
+        appBar: AppBar(title: const Text('Modify')),
+        body: Column(children: [
+          ElevatedButton(
+            child: const Text('Add item'),
+            onPressed: () => addItem('item'),
           ),
-          ElevatedButton(child:Text('Add More'), onPressed:(){Navigator.push(context,MaterialPageRoute(builder:(context)=>Todo()));}),
-          Text('Latest task: $name')
-        ]
-      )
-    );
-  }
-}
-
-class Todo extends ConsumerStatefulWidget{
-  const Todo({super.key});
-
-  @override
-  ConsumerState<Todo> createState()=> _TodoState();
-}
-
-class _TodoState extends ConsumerState<Todo>{
-  final TextEditingController _title= TextEditingController();
-  final TextEditingController _value=TextEditingController();
-  void addToList(){
-    final old_list=ref.read(listProvider);
-    ref.read(listProvider.notifier).state=[...old_list,{_title.text:_value.text}];
-    final box = Hive.box('myBox');
-    box.put('task', _title.text);
-    //box.delete('name'); // Removes key 'name'
-    //box.clear();        // Clears entire box
-    _title.clear();
-    _value.clear();
-  }
-  @override
-  Widget build(BuildContext context){
-    final list= ref.watch(listProvider);
-    return Scaffold(
-      appBar:AppBar(title:Text('TODO ADD')),
-      body:Column(
-        children:[
-          TextField(controller:_title),
-          Text('its value:'),
-          TextField(controller:_value),
-          ElevatedButton(child:Text('add'),onPressed:addToList)
-        ]
-      )
-    );
+          ElevatedButton(
+            child: const Text('Remove item'),
+            onPressed: () => deleteItem('item'),
+          )
+        ]));
   }
 }
